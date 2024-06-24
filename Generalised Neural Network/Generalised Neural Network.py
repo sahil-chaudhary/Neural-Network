0388e7f64684c948431d43945d42b13ae9acef0d
Sahil Chaudhary <63643403+sahil-chaudhary@users.noreply.github.com>
2024-06-24 11:59:36 +0530

    Update Generalised Neural Network.py

--- Generalised Neural Network/Generalised Neural Network.py
+++ Generalised Neural Network/Generalised Neural Network.py
@@ -5,10 +5,10 @@ import torch
 
 class forward_Prop:
     #The idea is to generalised the idea of forward propogation for any number of layers and nodes for the neural network
-    def __init__(self, x, y, parameters):
+    def __init__(self, x, y, b):
         self.x = x
         self.y = y
-        self.parameters = parameters
+        self.b = b
 
     def linear(self, x, w, b):
         return np.dot(x, w) + b
@@ -39,6 +39,7 @@ class back_Prop:
         self.parameters = parameters
 
     def linear_backward(self, dz, x, w, b):
+        #dz is the gradient of the loss function with respect to the output of the linear layer which happens to be same as the input of the activation function
         dw = np.dot(x.T, dz)
         db = np.sum(dz, axis=0)
         dx = np.dot(dz, w.T)
@@ -79,9 +80,9 @@ class optimiser:
 
 class error:
     #This class will take care of the different error functions that can be used to evaluate the performance of the neural network. Later, we will compare the performance of different error functions on the same neural network and make some conclusions.
-    def __init__(self) -> None:
-        
-        
+    def __init__(self):
+        pass
+
     def mean_squared_error(self, y_pred, y_true):
         return np.mean((y_pred - y_true)**2)
     
@@ -158,34 +159,62 @@ class accuracy:
 class Generalised_Neural_Network:
     #Ofcourse, the main class that will take care of the neural network. This is the class where we will call the above classes to train the model and make predictions.
     # This should also take care of number of layers and nodes in each layer
-    def __init__(self, x, y, parameters, learning_rate, epochs, nodes_num, layers_num, activation_functions, optimisation_algorithm, error_function):
+    def __init__(self, x, y, learning_rate, epochs, nodes_num, layers_num, activations, optimiser, error):
         self.x = x
         self.y = y
-        self.parameters = parameters
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.nodes_num = nodes_num
         self.layers_num = layers_num
-        self.activation_functions = activation_functions
-        self.optimisation_algorithm = optimisation_algorithm
-        self.error_function = error_function
+        self.activations = activations
+        self.optimiser = optimiser
+        self.error = error
 
-    def train(self, x, y, parameters, learning_rate, epochs):
-        pass
-
-    def save(self, path):
-        pass
-
-
-class init:
-    #This class will take care of the initialisation of the weights and biases of the neural network.
-    def __init__(self, x, nodes_num, layers_num):
-        self.x = x
+class parameter_initialisation:
+    def __init__(self, nodes_num, layers_num, x, y):
         self.nodes_num = nodes_num
         self.layers_num = layers_num
-        
-    def random(self, x, nodes_num, layers_num):
-        pass
+        self.x = x
+
+    #The idea is to create 'n+2' layers with 'd' nodes in each layer. For this, w_0 \in R^{m x d}[## I am expecting the input to be m x 1] and b_0 \in R^{d x 1} and w_1 \in R^{d x d} and b_1 \in R^{d x 1} and so on till w_{n} \in R^{d x d} and b_{n} \in R^{d x 1} and w_{n+1} \in R^{d x k} and b_{n+1} \in R^{k x 1} where k is the number of classes in the classification problem
+
+    # 1. w={w_0,w_1,...,w_{n},w_{n+1}} and b={b_0,b_1,...,b_{n},b_{n+1}}
+    # 2. a={a_0,a_1,...,a_{n},a_{n+1}} , a_0 till a_{n} \in R^{d x 1} and a_{n+1} \in R^{k x 1}
+    # 3. z={z_0,z_1,...,z_{n},z_{n+1},z_{n+2}} , z_0 till z_{n+1} \in R^{d x 1} and z_{n+2} \in R^{k x 1}
+    # 4. where z_0=x and a_0=w_0^T.x+b_0. Similarly, z_1=h(a_0) and a_1=w_1^T.z_1+b_1 and so on till z_{n+2}=h(a_{n+1}) where h is the activation function
+
+
+    def weights_and_biases_initialisation(self, x, y, nodes_num, layers_num):
+        weights = {}
+        biases = {}
+        for i in range(layers_num+1):
+            if i == 0:
+                weights['w_0'] = np.random.randn(x.shape[1], nodes_num)
+                biases['b_0'] = np.random.randn(1, nodes_num)
+            elif i == layers_num:
+                weights['w_'+str(i)] = np.random.randn(nodes_num, y.shape[1])
+                biases['b_'+str(i)] = np.random.randn(1, y.shape[1])
+            else:
+                weights['w_'+str(i)] = np.random.randn(nodes_num, nodes_num)
+                biases['b_'+str(i)] = np.random.randn(1, nodes_num)
+
+        return weights, biases
+    
+    def activation_initialisation(self, x, y, nodes_num, layers_num):
+        z={}
+        for i in range(layers_num+2):
+            if i == 0:
+                z['z_0'] = x
+            else:
+                z['z_'+str(i)] = np.zeros((x.shape[0], nodes_num))
+        return z
+
+    
+    def layer_forward_linear_initialisation(self, x, y, nodes_num, layers_num):
+        a={}
+        for i in range(layers_num+1):
+                a['a_'+str(i)] = np.zeros((x.shape[0], nodes_num)) #we will evaluate in network class using forward class
+        return a
 
     
 
