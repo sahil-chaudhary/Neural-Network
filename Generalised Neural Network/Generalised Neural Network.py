2ff965b86822c21dc71df775829a2dd09fa5e90d
Sahil Chaudhary <63643403+sahil-chaudhary@users.noreply.github.com>
2024-07-21 23:52:07 +0530

    Add files via upload

--- Generalised Neural Network/Generalised Neural Network.py
+++ Generalised Neural Network/Generalised Neural Network.py
@@ -1,290 +1,191 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import torch
+from keras.datasets import mnist
+from keras.utils import to_categorical
 
+# Load and preprocess the MNIST dataset
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], -1).T / 255.0
+X_test = X_test.reshape(X_test.shape[0], -1).T / 255.0
+Y_train = to_categorical(Y_train).T
+Y_test = to_categorical(Y_test).T
 
-class forward_Prop:
-    #The idea is to generalised the idea of forward propogation for any number of layers and nodes for the neural network
-    def __init__(self, x, y, b):
-        self.x = x
-        self.y = y
-        self.b = b
+# Initialize parameters for neural network
+def initialize_parameters(layer_dims):
+    np.random.seed(1)
+    parameters = {}
+    for l in range(1, len(layer_dims)):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * np.sqrt(2 / layer_dims[l - 1])  # He initialization
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+    return parameters
 
-    def linear(self, x, w, b):
-        return np.dot(x, w) + b
+# Activation functions and their derivatives
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
+
+def relu(Z):
+    return np.maximum(0, Z)
+
+def tanh(Z):
+    return np.tanh(Z)
+
+def softmax(Z):
+    expZ = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+    return expZ / expZ.sum(axis=0, keepdims=True)
+
+def sigmoid_backward(dA, Z):
+    sig = sigmoid(Z)
+    return dA * sig * (1 - sig)
+
+def relu_backward(dA, Z):
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    return dZ
+
+def tanh_backward(dA, Z):
+    return dA * (1 - np.tanh(Z)**2)
+
+# Mean Squared Error Loss and its derivative
+def mean_squared_error(Y_hat, Y):
+    m = Y.shape[1]
+    return np.sum((Y_hat - Y)**2) / (2 * m)
+
+def mean_squared_error_backward(Y_hat, Y):
+    m = Y.shape[1]
+    return (Y_hat - Y) / m
+
+# Linear backward step
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
     
-class activation:
-    #The idea is to generalised the idea of activation functions for any number of layers and nodes for the neural network
-    def __init__(self, x):
-        self.x = x
-
-    def sigmoid(self, x):
-        return 1/(1 + np.exp(-x))
-
-    def relu(self, x):
-        return np.maximum(0, x)
-
-    def tanh(self, x):
-        return np.tanh(x)
-
-    def softmax(self, x):
-        return np.exp(x)/np.sum(np.exp(x), axis=0)
-
-class back_Prop:
-    #The idea is to generalised the idea of back propogation for any number of layers and nodes for the neural network. This class will take care of finding gradients of the loss function with respect to the weights and biases of the neural network using the chain rule of differentiation
-    def __init__(self, x, y, b):
-        self.x = x
-        self.y = y
-        self.b = b
-
-    def linear_backward(self, dz, x, w, b):
-        #dz is the gradient of the loss function with respect to the output of the linear layer which happens to be same as the input of the activation function
-        dw = np.dot(x.T, dz)
-        db = np.sum(dz, axis=0)
-        dx = np.dot(dz, w.T)
-        return dx, dw, db
+    dW = np.dot(dZ, A_prev.T) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    dA_prev = np.dot(W.T, dZ)
     
-    def activation_backward(self, dz, x, activation):
-        if activation == 'sigmoid':
-            return dz*activation.sigmoid(x)*(1-activation.sigmoid(x))
-        elif activation == 'relu':
-            return np.where(x>0, dz, 0)
-        elif activation == 'tanh':
-            return 1 - np.tanh(x)**2
-        elif activation == 'softmax':
-            return dz*activation.softmax(x)*(1-activation.softmax(x))
+    return dA_prev, dW, db
 
-class optimiser:
-    #This will take care of the collection of optimisation algorithms that can be used to update the weights and biases of the neural network. Later, we will compare the performance of different optimisation algorithms on the same neural network and make some conclusions.
-    def __init__(self, learning_rate):
-        self.learning_rate = learning_rate
-
-    def gradient_descent(self, weights, biases, dw, db):
-        weights -= self.learning_rate*dw
-        biases -= self.learning_rate*db
-        return weights, biases
+# Activation backward
+def activate_backward(dA, cache, activation):
+    linear_cache, activation_cache = cache
     
-    def momentum(self, weights, biases, dw, db, beta):
-        v_w = beta*v_w + (1-beta)*dw
-        v_b = beta*v_b + (1-beta)*db
-        weights -= self.learning_rate*v_w
-        biases -= self.learning_rate*v_b
-        return weights, biases
+    if activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+    elif activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+    elif activation == "tanh":
+        dZ = tanh_backward(dA, activation_cache)
     
-    def rmsprop(self, weights, biases, dw, db, beta):
-        s_w = beta*s_w + (1-beta)*dw**2
-        s_b = beta*s_b + (1-beta)*db**2
-        weights -= self.learning_rate*dw/np.sqrt(s_w + 1e-8)
-        biases -= self.learning_rate*db/np.sqrt(s_b + 1e-8)
-        return weights, biases
+    return linear_backward(dZ, linear_cache)
+
+# Full backward propagation
+def backward_propagation(AL, Y, caches, activations):
+    grads = {}
+    L = len(caches)  # number of layers
+    m = Y.shape[1]
+    Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
+
+    # Initializing the backpropagation
+    dAL = mean_squared_error_backward(AL, Y)
+
+    # Lth layer (SOFTMAX -> LINEAR) gradients
+    current_cache = caches[L - 1]
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = activate_backward(dAL, current_cache, activations[L - 1])
+
+    # Loop from l=L-2 to l=0
+    for l in reversed(range(L - 1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = activate_backward(grads["dA" + str(l + 2)], current_cache, activations[l])
+        grads["dA" + str(l + 1)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+# Training loop
+def model(X, Y, layer_dims, activations, optimizer, learning_rate=0.01, beta=0.9, beta1=0.9, beta2=0.999, epsilon=1e-8, num_epochs=1000, print_cost=False):
+    costs = []
+    parameters = initialize_parameters(layer_dims)
     
-    def adam(self, weights, biases, dw, db, beta1, beta2):
-        v_w = beta1*v_w + (1-beta1)*dw
-        v_b = beta1*v_b + (1-beta1)*db
-        s_w = beta2*s_w + (1-beta2)*dw**2
-        s_b = beta2*s_b + (1-beta2)*db**2
-        v_w_corrected = v_w/(1-beta1)
-        v_b_corrected = v_b/(1-beta1)
-        s_w_corrected = s_w/(1-beta2)
-        s_b_corrected = s_b/(1-beta2)
-        weights -= self.learning_rate*v_w_corrected/np.sqrt(s_w_corrected + 1e-8)
-        biases -= self.learning_rate*v_b_corrected/np.sqrt(s_b_corrected + 1e-8)
-        return weights, biases
+    if optimizer == "momentum":
+        v = initialize_velocity(parameters)
+    elif optimizer == "rmsprop":
+        s = initialize_rmsprop(parameters)
+    elif optimizer == "adam":
+        v, s = initialize_adam(parameters)
     
-
-class error:
-    #This class will take care of the different error functions that can be used to evaluate the performance of the neural network. Later, we will compare the performance of different error functions on the same neural network and make some conclusions.
-    def __init__(self):
-        pass
-
-    def mean_squared_error(self, y_pred, y_true):
-        return np.mean((y_pred - y_true)**2)
-    
-    def cross_entropy_error(self, y_pred, y_true):
-        return -np.sum(y_true*np.log(y_pred))
-    
-class predict:
-    #This class will take care of the prediction of the neural network. 
-    def __init__(self, x, parameters):
-        self.x = x
-        self.parameters = parameters
-
-    def predict(self, x):
-        return np.argmax(x, axis=1)
-    
-class plot:
-    #This class will take care of the plotting of the neural network. This class will have the following methods:
-    #1. plot_loss : This will plot the loss function of the neural network
-    #2. plot_accuracy : This will plot the accuracy of the neural network
-    #3. plot : This will plot both the loss function and the accuracy of the neural network
-    #4. plot_decision_boundary : This will plot the decision boundary of the neural network. This will be useful for the classification problems. This will help us to visualise how the neural network is making the decision on the basis of the input features.
-    #5. plot_confusion_matrix : This will plot the confusion matrix of the neural network. This will help us to visualise how the neural network is performing on the basis of the true labels and the predicted labels.
-    def __init__(self, x, y, parameters):
-        self.x = x
-        self.y = y
-        self.parameters = parameters
-
-    def plot_loss(self, loss):
-        plt.plot(loss)
-        plt.xlabel('Epochs')
-        plt.ylabel('Loss')
-        plt.title('Loss Function')
-        plt.show()
-
-    def plot_accuracy(self, accuracy):
-        plt.plot(accuracy)
-        plt.xlabel('Epochs')
-        plt.ylabel('Accuracy')
-        plt.title('Accuracy')
-        plt.show()
-
-    def plot(self, loss, accuracy):
-        plt.plot(loss, label='Loss')
-        plt.plot(accuracy, label='Accuracy')
-        plt.xlabel('Epochs')
-        plt.ylabel('Loss/Accuracy')
-        plt.title('Loss/Accuracy')
-        plt.legend()
-        plt.show()
-
-    def plot_decision_boundary(self, x, y, parameters):
-        pass
-
-    def plot_confusion_matrix(self, y_true, y_pred):
-        pass
-
-class accuracy:
-    #This class will take care of the accuracy of the neural network.
-    def __init__(self, y_pred, y_true):
-        self.y_pred = y_pred
-        self.y_true = y_true
-
-    def accuracy(self, y_pred, y_true):
-        return np.mean(y_pred == y_true)
-    
-class Generalised_Neural_Network:
-    #Ofcourse, the main class that will take care of the neural network. This is the class where we will call the above classes to train the model and make predictions.
-    # This should also take care of number of layers and nodes in each layer
-    def __init__(self, x, y, learning_rate, epochs, nodes_num, layers_num, activations,  optimiser, error, weights, biases, a, z):
-        self.x = x
-        self.y = y
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-        self.nodes_num = nodes_num
-        self.layers_num = layers_num
-        self.activations = activations
-        self.optimiser = optimiser
-        self.error = error
-        self.weights = weights
-        self.biases = biases
-        self.a = a
-        self.z = z
-
-    def update_parameters(self, x, y, weights, biases, a, z, nodes_num, layers_num, activations):
-        for i in range(layers_num+1):
-            forward_Prop1 = forward_Prop(x, y, biases['b_'+str(i)])
-            a['a_'+str(i)] = forward_Prop1.linear(z['z_'+str(i)], weights['w_'+str(i)], biases['b_'+str(i)])
-            activation = activation(a['a_'+str(i)])
-            if activations[i] == 'sigmoid':
-                z['z_'+str(i+1)] = activation.sigmoid(a['a_'+str(i)])
-            elif activations[i] == 'relu':
-                z['z_'+str(i+1)] = activation.relu(a['a_'+str(i)])
-            elif activations[i] == 'tanh':
-                z['z_'+str(i+1)] = activation.tanh(a['a_'+str(i)])
-            elif activations[i] == 'softmax':
-                z['z_'+str(i+1)] = activation.softmax(a['a_'+str(i)])
-        print('done with update parameters')
-        return a, z
-    
-
-    
-class parameter_initialisation:
-    def __init__(self, x, y, nodes_num, layers_num):
-        self.nodes_num = nodes_num
-        self.layers_num = layers_num
-        self.x = x
-        self.y = y
-
-    #The idea is to create 'n+2' layers with 'd' nodes in each layer. For this, w_0 \in R^{m x d}[## I am expecting the input to be m x 1] and b_0 \in R^{d x 1} and w_1 \in R^{d x d} and b_1 \in R^{d x 1} and so on till w_{n} \in R^{d x d} and b_{n} \in R^{d x 1} and w_{n+1} \in R^{d x k} and b_{n+1} \in R^{k x 1} where k is the number of classes in the classification problem
-
-    # 1. w={w_0,w_1,...,w_{n},w_{n+1}} and b={b_0,b_1,...,b_{n},b_{n+1}}
-    # 2. a={a_0,a_1,...,a_{n},a_{n+1}} , a_0 till a_{n} \in R^{d x 1} and a_{n+1} \in R^{k x 1}
-    # 3. z={z_0,z_1,...,z_{n},z_{n+1},z_{n+2}} , z_0 till z_{n+1} \in R^{d x 1} and z_{n+2} \in R^{k x 1}
-    # 4. where z_0=x and a_0=w_0^T.x+b_0. Similarly, z_1=h(a_0) and a_1=w_1^T.z_1+b_1 and so on till z_{n+2}=h(a_{n+1}) where h is the activation function
-
-
-    def weights_and_biases_initialisation(self, x, y, nodes_num, layers_num):
-        weights = {}
-        biases = {}
-        for i in range(layers_num+1):
-            if i == 0:
-                weights['w_0'] = np.random.randn(x.shape[1], nodes_num)
-                biases['b_0'] = np.random.randn(1, nodes_num)
-            elif i == layers_num:
-                weights['w_'+str(i)] = np.random.randn(nodes_num, y.shape[1])
-                biases['b_'+str(i)] = np.random.randn(1, y.shape[1])
-            else:
-                weights['w_'+str(i)] = np.random.randn(nodes_num, nodes_num)
-                biases['b_'+str(i)] = np.random.randn(1, nodes_num)
-        print('done with weights and biases initialisation')
-        return weights, biases
-    
-    def activation_initialisation(self, x, y, nodes_num, layers_num):
-        z={}
-        for i in range(layers_num+2):
-            if i == 0:
-                z['z_0'] = x
-            else:
-                z['z_'+str(i)] = np.zeros((x.shape[0], nodes_num))
-        print('done with activation initialisation')
-        return z
-
-    
-    def layer_forward_linear_initialisation(self, x, y, nodes_num, layers_num):
-        a={}
-        for i in range(layers_num+1):
-                a['a_'+str(i)] = np.zeros((x.shape[0], nodes_num)) #we will evaluate in network class using forward class
-        print('done with layer forward linear initialisation')
-        return a
-    
-    def activations_initialisation(self, x, y, nodes_num, layers_num):
-        activations = []
-        for i in range(layers_num):
-            activations.append('sigmoid')
-        activations.append('softmax')
-        print('done with activations initialisation')
-        return activations
+    for t in range(1, num_epochs + 1):
+        # Forward propagation
+        A = X
+        caches = []
+        for l in range(1, len(layer_dims)):
+            A_prev = A
+            W = parameters['W' + str(l)]
+            b = parameters['b' + str(l)]
+            A, cache = activate_forward(A_prev, W, b, activations[l-1])
+            caches.append(cache)
         
-    def dz_initialisation(x, y, nodes_num, layers_num):
-        dz = {}
-        for i in range(layers_num+1):
-            print('Layer:', i)
-            dz['dz_'+str(i)] = np.zeros((x.shape[0], nodes_num))
-        print('done with dz initialisation')
-        return dz
-
-    def da_initialisation(x, y, nodes_num, layers_num):
-        da = {}
-        for i in range(layers_num+1):
-            print('Layer:', i)
-            da['da_'+str(i)] = np.zeros((x.shape[0], nodes_num))
-        print('done with da initialisation')
-        return da
-
-    def dw_initialisation(x, y, nodes_num, layers_num):
-        dw = {}
-        for i in range(layers_num+1):
-            print('Layer:', i)
-            dw['dw_'+str(i)] = np.zeros((x.shape[1], nodes_num))
-        print('done with dw initialisation')
-        return dw
+        # Compute loss
+        loss = mean_squared_error(A, Y)
+        
+        # Backward propagation
+        grads = backward_propagation(A, Y, caches, activations)
+        
+        # Update parameters
+        if optimizer == "sgd":
+            parameters = update_parameters_sgd(parameters, grads, learning_rate)
+        elif optimizer == "momentum":
+            parameters, v = update_parameters_momentum(parameters, grads, v, beta, learning_rate)
+        elif optimizer == "rmsprop":
+            parameters, s = update_parameters_rmsprop(parameters, grads, s, beta, learning_rate, epsilon)
+        elif optimizer == "adam":
+            parameters, v, s = update_parameters_adam(parameters, grads, v, s, t, learning_rate, beta1, beta2, epsilon)
+        
+        # Print the cost every 100 iterations
+        if print_cost and t % 100 == 0:
+            print(f"Cost after epoch {t}: {loss}")
+            costs.append(loss)
     
+    return parameters, costs
 
+# Define the model architecture
+layer_dims = [784, 128, 64, 10]  # Input layer, two hidden layers, output layer
+activations = ["relu", "relu", "softmax"]
 
+# Train the model with Adam optimizer
+parameters, costs = model(X_train, Y_train, layer_dims, activations, optimizer="adam", learning_rate=0.001, num_epochs=2000, print_cost=True)
+
+# Plotting the cost function
+plt.plot(costs)
+plt.ylabel('Cost')
+plt.xlabel('Iterations (per hundreds)')
+plt.title('Cost reduction over time')
+plt.show()
+
+# Predict on the test set
+def predict(X, parameters, activations):
+    A = X
+    L = len(parameters) // 2  # number of layers in the neural network
+    caches = []
     
-
+    for l in range(1, L + 1):
+        A_prev = A
+        W = parameters['W' + str(l)]
+        b = parameters['b' + str(l)]
+        A, cache = activate_forward(A_prev, W, b, activations[l-1])
+        caches.append(cache)
     
+    return A
 
+Y_pred = predict(X_test, parameters, activations)
 
+# Compute accuracy
+def compute_accuracy(Y_pred, Y_true):
+    predictions = np.argmax(Y_pred, axis=0)
+    labels = np.argmax(Y_true, axis=0)
+    accuracy = np.mean(predictions == labels)
+    return accuracy
 
-
+accuracy = compute_accuracy(Y_pred, Y_test)
+print(f"Test set accuracy: {accuracy * 100:.2f}%")
